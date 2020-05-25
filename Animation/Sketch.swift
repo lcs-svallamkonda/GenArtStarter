b4d493e3d228a7de270fc0ca01f656bb3a66637c
Russell Gordon <rgordon@lcs.on.ca>
2020-05-25 09:34:04 -0400

    Draws agent at centre of screen, unmoving.

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -7,34 +7,30 @@ class Sketch : NSObject {
     //       Therefore, the line immediately below must always be present.
     let canvas : Canvas
     
-    // Tortoise to draw with
-    let turtle : Tortoise
-        
+    // Agent centre point
+    let centre: Point
+    let radius: Int
+    
     // This function runs once
     override init() {
         
         // Create canvas object – specify size
         canvas = Canvas(width: 500, height: 500)
         
-        // Create turtle to draw with
-        turtle = Tortoise(drawingUpon: canvas)
+        // No fill on canvas
+        canvas.drawShapesWithFill = false
         
-        // Move to middle of screen
-        turtle.penUp()
-        turtle.setPosition(to: Point(x: 250, y: 250))
-        turtle.penDown()
-
+        // Define properties of the agent
+        centre = Point(x: canvas.width / 2, y: canvas.height / 2)
+        radius = 20
+        
     }
     
     // This function runs repeatedly, forever, to create the animated effect
     func draw() {
-        
-        // Required to bring canvas into same orientation and origin position as last run of draw() function
-        turtle.restoreStateOnCanvas()
 
-        // Move the turtle forward and turn it a bit
-        turtle.forward(steps: 5)
-        turtle.right(by: 1)
+        // Draw a circle at this point
+        canvas.drawEllipse(at: centre, width: radius * 2, height: radius * 2)
         
     }
     
